71a09e2b003a462f4948c406ced9086357ed79d7
Aishwarya Iyer <74247717+Aishwarya2701@users.noreply.github.com>
2024-06-10 18:45:28 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,27 @@
+import * as Speech from 'expo-speech';
+import * as React from 'react';
+import { Button, StyleSheet, Text, View } from 'react-native';
+export default function App() {
+  const thingToSay = "I am beginning to feel like a Rap God, Rap God All my people from the front to the back nod, back nod Now, who thinks their arms are long enough To slap box, slap box ? They said I rap like a robot, so call me Rap - bot";
+  const speak = () => {
+    Speech.speak(thingToSay);
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.Texts}>{thingToSay}</Text>
+      <Button title="Start listening" onPress={speak} />
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    backgroundColor: '#ecf0f1',
+    padding: 8,
+  },
+  Texts: {
+    marginTop: 10,
+    color: 'black',
+    padding: 10,
+}})
